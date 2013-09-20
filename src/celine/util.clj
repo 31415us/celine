36fe9e7d499f8af615421ff71f4362ffa20a98e5
31415us <pius.vondaeniken@epfl.ch>
2013-09-20 20:21:11 +0200

    made unfold tail-recursive to avoid stack-overflow

--- src/celine/util.clj
+++ src/celine/util.clj
@@ -1,13 +1,19 @@
 
 (ns celine.util)
 
-(defn unfold [seed step end-predicate]
+(defn unfold [_seed _step _end-predicate]
   "creates a sequence of values from a seed value 
   by repeatedly applying the step function to it
   until it satisfies the end-predicate"
-  (if (end-predicate seed)
-    (list seed)
-    (cons seed (unfold (step seed) step end-predicate))
+  (loop [seed _seed
+         step _step
+         end _end-predicate
+         acc ()
+        ]
+    (if (end seed)
+      (cons seed acc)
+      (recur (step seed) step end (cons seed acc))
+    )
   )
 )
 
