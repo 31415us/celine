c73df7741975431505253418e08a6964a8287bd3
31415us <pius.vondaeniken@epfl.ch>
2013-09-23 23:31:26 +0200

    all setup to start implementing

--- src/celine/shader.clj
+++ src/celine/shader.clj
@@ -1,4 +1,12 @@
 
-(ns celine.shader)
+(ns celine.shader
+  (:require [celine.bitmap :as bitmap])
+  (:require [celine.color :as color])
+  (:require [celine.geometry :as geom])
+  (:require [celine.scenery :as scene])
+  (:require [celine.util :as util])
+)
 
+
+(defn render-scene [scene])
 
